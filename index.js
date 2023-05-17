6cc77292e00af1678824c81fa6e9914688392a06
Ritesh18117 <rgriteshbyahut18117rg@gmail.com>
2023-05-17 16:07:23 +0530

    Adding News Page

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const url = process.env.url;
 const PORT = process.env.PORT || 5000;
 
@@ -5,9 +7,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 var request = require('request');
-
+// For Contact Submit
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
+
+// For News 
+const NewsAPI = require('newsapi');
+const { error } = require('console');
+const newsapi = new NewsAPI(`${process.env.news}`);
+
 
 // Middleware to parse the body of the incoming request
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,6 +30,19 @@ app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('/', (req,res) => {
     res.render('index.ejs')
+})
+
+app.get('/news', (req,res) => {
+  newsapi.v2.topHeadlines({
+    country:'in',
+    category:'business',
+  }).then(response => {
+    console.log(response);
+    res.render('news.ejs',{response})
+  }).catch(error =>{
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  });
 })
 
 app.get('/about', (req,res) => {
